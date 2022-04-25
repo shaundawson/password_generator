0296c49f2ef1673961f7eb3f597f24677c00fe9a
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-25 15:50:47 -0400

    high score exercise

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-# For Loop
+# FOR LOOPS
 # fruits = ["Apple", "Peach", "Pear"]
 # for fruit in fruits:
 #   print(fruit)
@@ -7,9 +7,9 @@
 
 # AVERAGE HEIGHT EXERCISE
 # Write a program that calculates the average student height from a List of heights without using sum() or len()
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
 #print(student_heights)
 
 # solution  using sum () and len()
@@ -19,14 +19,35 @@ for n in range(0, len(student_heights)):
 # print(avg_height
 
 # Replicate how the sum() works
-total_height = 0
-for height in student_heights:
-  total_height += height
+# total_height = 0
+# for height in student_heights:
+#   total_height += height
 
-#Replicate how len() works
-num_students = 0
-for student in student_heights:
-  num_students += 1
+# Replicate how len() works
+# num_students = 0
+# for student in student_heights:
+#   num_students += 1
 
-average_height = round(total_height / num_students)
-print(average_height)
+# average_height = round(total_height / num_students)
+# print(average_height)
+
+
+
+# HIGH SCORE EXERCISE
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+# Don't use the max() or min ()
+# print(max(student_scores))
+# print(min(student_scores))
+
+# Replicate how max works ()
+#Initialize the max value to the first element.
+max_score = student_scores[0]
+# Iterate through the list, and if we find a larger value than the current max, we assign that value to max
+for score in student_scores:
+  if score > max_score:
+    max_score = score
+print(f"The highest score in the class is: {max_score}")

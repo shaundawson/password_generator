5da5890be1677be0d1af6f91e581a3cc6d2c9c05
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-25 17:45:44 -0400

    password_generator, set variables for input

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the PyPassword Generator")
+number_of_letters = input("How many letters would you like in your password?\n")
+number_of_symbols = input("How many symbols would you like?\n")
+number_of_numbers = input("How many numbers would you like?\n")
+
+
+
+
+#print(f"Here is your password: {password}")
